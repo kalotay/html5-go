a142e1b210aba8fd517d72def4fcad71ffb64da6
Daniel Kalotay <d.kalotay@gmail.com>
2012-06-24 22:57:53 +0100

    start of layout
    dynamically generated grid complete
    basic stylesheet
    image files for grid complete

---
+++ go.js
@@ -0,0 +1,20 @@
+var BOARD_SIZE = 19;
+
+function createRow(parentDiv) {
+    var i;
+    var newPosition;
+    for (i = 0; i < BOARD_SIZE; ++i) {
+        newPosition = document.createElement("button");
+        parentDiv.appendChild(newPosition);
+    }
+}
+
+function createBoard() {
+    var i;
+    var newRow;
+    for (i = 0; i < BOARD_SIZE; ++i) {
+        newRow = document.createElement("div");
+        createRow(newRow);
+        document.body.appendChild(newRow);
+    }
+}
