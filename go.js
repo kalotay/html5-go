4e145208d19fb9a604d67b1ad245c51e1b7222bb
Daniel Kalotay <d.kalotay@gmail.com>
2012-06-25 02:59:37 +0100

    beginning of game logic
    so far only the interface is defined

--- go.js
+++ go.js
@@ -16,6 +16,44 @@ var Turner = (function () {
     return Turner;
 })()
 
+function Points(seedRows, seedColumns) {
+    this.rows = seedRows;
+    this.columns = seedColumns;
+}
+
+function addPoint(row, column) {
+    this.rows.push(row);
+    this.columns.push(columns);
+}
+
+Points.prototype.add = addPoint;
+
+function Shape(seedRow, seedColumn, otherShapes) {
+    this.stones = new Points([seedRow], [seedColumn]);
+    this.liberties = [];
+    this.setLiberties(otherShapes);
+}
+
+function stoneInShape(row, column) {
+}
+
+Shape.prototype.isStone(row, column) = stoneInShape;
+
+function libertyInShape(row, column) {
+}
+
+Shape.prototype.isLiberty(row, column) = libertyInShape;
+
+function addToShape(row, column) {
+}
+
+Shape.prototype.add(row, column) = addToShape;
+
+function setLiberties(otherShapes) {
+}
+
+Shape.prototype.setLiberties(otherShapes) = setLiberties;
+
 function onButtonPress(clickEvent) {
     var button = clickEvent.target;
     if (button.value !== "free") {
@@ -32,6 +70,7 @@ function createRow(parentDiv) {
         newPosition = document.createElement("button");
         newPosition.type = "button";
         newPosition.value = "free";
+        newPosition.className = i.toString(10);
         newPosition.addEventListener("click", onButtonPress);
         parentDiv.appendChild(newPosition);
     }
@@ -42,6 +81,7 @@ function createBoard() {
     var newRow;
     for (i = 0; i < BOARD_SIZE; ++i) {
         newRow = document.createElement("div");
+        newRow.className = i.toString(10);
         createRow(newRow);
         document.body.appendChild(newRow);
     }
