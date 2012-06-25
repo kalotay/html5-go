0be4a82a243f3f85fde2e6d02c9618d9a2b5fc84
Daniel Kalotay <d.kalotay@gmail.com>
2012-06-25 01:58:54 +0100

    players can now put pieces down

--- go.js
+++ go.js
@@ -1,11 +1,35 @@
 var BOARD_SIZE = 19;
+//closures ftw
+var Turner = (function () {
+    var turn = "black";
+    var Turner = {};
+    Turner.turn = function () {
+        if (turn === "black") {
+            turn = "white";
+        } else {
+            turn = "black";
+        }
+    };
+    Turner.get = function () {
+        return turn;
+    };
+    return Turner;
+})()
+
+function onButtonPress(clickEvent) {
+    var button = clickEvent.target;
+    button.value = Turner.get();
+    Turner.turn();
+}
 
 function createRow(parentDiv) {
     var i;
     var newPosition;
     for (i = 0; i < BOARD_SIZE; ++i) {
-        newPosition = document.createElement("img");
-        newPosition.src = "intersect.png";
+        newPosition = document.createElement("button");
+        newPosition.type = "button";
+        newPosition.value = "free";
+        newPosition.addEventListener("click", onButtonPress);
         parentDiv.appendChild(newPosition);
     }
 }
